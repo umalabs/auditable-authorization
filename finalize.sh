aca499ef0c452303c3e2e68333b2fdc4bd2c2af5
1token <zboran@federizer.com>
2022-04-13 13:08:18 +0200

    Auditable Authorization.pdf

--- finalize.sh
+++ finalize.sh
@@ -1,1 +1,1 @@
-gs -dSAFER -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile=Auditable Authorization.pdf README.pdf pdfmarks
+gs -dSAFER -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile=Auditable_Authorization.pdf README.pdf pdfmarks
